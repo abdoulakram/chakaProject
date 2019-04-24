a49dd427fa6eddc8f26f2502fa9a895221ffc3a1
abdoulakram <nabdoulakram@gmail.com>
2019-04-24 09:45:35 +0000

    commit

--- pfe/password1/app.py
+++ pfe/password1/app.py
@@ -28,8 +28,8 @@ def retrievePassWord():
 def envoi():
     if request.method == 'POST':
         password = request.form['password']
-    account_sid = 'AC89c7cf15d429617da0f4dbe4ad393744'
-    auth_token = '75b6f0ce16d7b0b713aaf7d70a11605e'
+    account_sid = 'ACfd1cd064b3524fc490613ef53a0b8c77'
+    auth_token = '9d3b3368116db94b5ff565012ef31ac4'
     client = Client(account_sid, auth_token)
     
     url = 'https://inputpass.chakamobile.com/?sessionid=idsessiontest4'
