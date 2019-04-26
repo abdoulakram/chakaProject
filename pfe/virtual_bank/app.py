d123c90b0aab8192d2dfb730a50f24bb54776507
abdoulakram <nabdoulakram@gmail.com>
2019-04-26 16:00:31 +0000

    commit

--- pfe/virtual_bank/app.py
+++ pfe/virtual_bank/app.py
@@ -69,7 +69,7 @@ def sms_reply():
         for i in range(len(liste)):
             chaine+=liste[i]+"\n"
         if(str(chaine).__contains__("SECRET")):
-            resp.message(str(chaine.replace("b'",""))+str("\nhttps://inputpass.chakamobile.com/?sessionid="+sessionid+"&phone="+phone_no))
+            resp.message(str(chaine.replace("b'",""))+str("\nhttps://inputpass.chakamobile.com/?sessionid="+str(sessionid)+"&phone="+phone_no))
             
         else:
             resp.message(str(chaine.replace("b'","")))
