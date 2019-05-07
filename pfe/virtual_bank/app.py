5398b8041b6a6cc0b69a9b82b0717e6215b405af
abdoulakram <nabdoulakram@gmail.com>
2019-05-07 11:27:18 +0000

    commit

--- pfe/virtual_bank/app.py
+++ pfe/virtual_bank/app.py
@@ -2,26 +2,16 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import urllib.parse
 from urllib.request import Request, urlopen
-from datetime import datetime
-import random
-import string
 import mysql.connector
+import getRespFromUrl
 
-#import MySQLdb
-
-#sessionid="idsessiontest4"#datetime.now().strftime("%d-%b-%Y-%H:%M:%S.%f") 
 
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['POST'])
 
-def sms_reply():
-    #global sessionid
-   # letters = string.ascii_lowercase+string.digits
-    
-    #rand_letters = "".join(choices(letters,k=20))
-    
+def sms_reply(): 
     
     phone_no = request.form.get('From')
     num=str(phone_no[10:])
@@ -43,45 +33,10 @@ def sms_reply():
     cursor.close()
     
     mySQL_conn.close()
+
+    url='http://10.10.180.167:7092/mtncg/requests'
     
-    headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-                      'AppleWebKit/537.11 (KHTML, like Gecko) '
-                      'Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'
-        }
-    req = Request('http://10.10.180.167:7092/mtncg/requests', headers=headers)
-    params={
-    "sessionid":sessionid,
-    "msisdn":"242055565990",
-    "input":msg
-            }
-    
-    query_string=urllib.parse.urlencode(params)
-    data=query_string.encode("ascii")
-    with urllib.request.urlopen(url=req,data=data) as response:
-        
-        response_text=response.read()
-       # header=response.headers  
-   
-        response_text_str=str(response_text)
-        liste=response_text_str.split("\\n")
-       
-    
-        liste.remove("'")
-        chaine=""
-        for i in range(len(liste)):
-            chaine+=liste[i]+"\n"
-        if(str(chaine).__contains__("SECRET")):
-            resp.message(str(chaine.replace("b'",""))+str("\nhttps://inputpass.chakamobile.com/?token="+str(sessionid)))
-            
-        else:
-            resp.message(str(chaine.replace("b'","")))
-        
-        return str(resp)
+    return getRespFromUrl.urlChange(msg,sessionid,url,resp)
    
 
 if __name__ == "__main__":
